3defd94d3220a98147ec9d9f8cdbf37744211f63
archibate <1931127624@qq.com>
2021-08-07 12:07:51 +0800

    clean

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	PYTHONPATH=external/zeno cmake -B build
+	cmake -B build
 	make -C build -j 32
 
 run:
-	PYTHONPATH=external/zeno python -m zenqt
+	python run.py
--- run.py
+++ run.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
 import runpy
 
-sys.path.insert(0, 'external/zeno')
-try:
-    from zenqt.main import main
-    sys.exit(main())
-except ImportError as e:
-    raise ImportError('please run `git submodule update --init --recursive`') from e
+runpy.run_path('external/zeno/run.py')
